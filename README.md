c20990bfc7ca1635ddffbd0e3ac326915d37aa4d
Clayton Moura <clee.gomes@gmail.com>
2024-07-24 23:39:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Desafio Controle de Fluxo
+
+Neste desafio, fui responsável por modelar e diagramar a representação UML do componente iPhone, abrangendo suas funcionalidades como Reprodutor Musical, Aparelho Telefônico e Navegador na Internet.
+
+Contexto
+Com base no vídeo de lançamento do iPhone de 2007 [Link do Video](https://www.youtube.com/watch?v=9ou608QQRq8), elaborei a diagramação das classes e interfaces utilizando uma ferramenta UML de minha preferência. Em seguida, implementei as classes e interfaces no formato de arquivos .java.
+
+## 🛠️ Construído com
+
+* [Vscode](https://code.visualstudio.com/) 
+
+---
+⌨️ com ❤️ por [Clayton Moura](https://github.com/claytonmoura) 😊
---
+++ src/Iphone.java
@@ -0,0 +1,47 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Musica pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Musica selecionada");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("ligando para numero");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligaçao");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina da web");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizanod pagina");
+    }
+}
